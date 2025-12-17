f93e7367fc4cb4b5f8ba8b0f5d9213a5357c1330
Shreyachoudhary25 <shreya123slm@gmail.com>
2025-12-18 00:11:01 +0530

    Add EmployerProfile model and register in admin

--- users/models.py
+++ users/models.py
@@ -15,4 +15,18 @@ class StudentProfile(models.Model):
 
     def __str__(self):
         return self.full_name
+    
+class EmployerProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
+    company_name = models.CharField(max_length=150)
+    industry = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+
+    website = models.URLField(blank=True)
+    description = models.TextField(blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.company_name
