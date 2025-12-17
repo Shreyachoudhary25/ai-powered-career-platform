d9c484cc0a34b3670557c3db9eeae4fb6dd98971
Shreyachoudhary25 <shreya123slm@gmail.com>
2025-12-18 00:48:14 +0530

    Fix admin registration typo for MentorProfile

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import StudentProfile, EmployerProfile
+from .models import StudentProfile, EmployerProfile, MentorProfile
 
 admin.site.register(StudentProfile)
 admin.site.register(EmployerProfile)
+admin.site.register(MentorProfile)
