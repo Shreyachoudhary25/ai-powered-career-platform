b0a7ba948bbb9724c2e1066e08e95a1ccb066bd7
Shreyachoudhary25 <shreya123slm@gmail.com>
2025-12-22 07:20:06 +0530

    Fix ApplicationSerializer by removing invalid created_at field

--- opportunities/serializers.py
+++ opportunities/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Job
+from .models import Application
 
 
 class JobSerializer(serializers.ModelSerializer):
@@ -14,3 +15,16 @@ class InternshipSerializer(serializers.ModelSerializer):
     class Meta:
         model = Internship
         fields = '__all__'
+
+class ApplicationSerializer(serializers.ModelSerializer):
+    student_name = serializers.CharField(source='student.full_name', read_only=True)
+    job_title = serializers.CharField(source='job.title', read_only=True)
+
+    class Meta:
+        model = Application
+        fields = [
+            'id',
+            'student_name',
+            'job_title',
+            'status',
+        ]
